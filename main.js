430012ac31fed90757798e5e920bf379fb642d36
unknown <sonwj0915@naver.com>
2021-05-19 22:16:07 +0900

    리사이즈 이벤트

--- main.js
+++ main.js
@@ -8,9 +8,21 @@
     toggleElements();
   });
 
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 1024) {
+      offElements();
+    }
+  });
+
   function toggleElements() {
     [].forEach.call($toggles, (toggle) => {
       toggle.classList.toggle("on");
+    });
+  }
+
+  function offElements() {
+    [].forEach.call($toggles, (toggle) => {
+      toggle.classList.remove("on");
     });
   }
 })(window, document);
